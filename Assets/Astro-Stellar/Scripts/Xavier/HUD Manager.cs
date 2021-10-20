cd1eb5e547e19e84fe6486c70115150475731784
Xavier Arias <xavier_rate@outlook.com>
2021-10-20 19:02:43 +1100

    A offline scene update.

---
+++ Assets/Astro-Stellar/Scripts/Xavier/HUD Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Astro_Stellar.Scripts.Xavier
+{
+    public class HUD_Manager : MonoBehaviour
+    {
+
+
+        IEnumerator TextPopUp
+        {
+            
+            yield return null;
+        }
+    }
+}
