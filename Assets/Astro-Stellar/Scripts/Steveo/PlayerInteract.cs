83a4d9f5c4844002609b8383ff62482b05900270
SteveoBurns <steveo.burns.777@gmail.com>
2021-10-27 10:53:11 +1100

    Cleaning scripts and Item prefabs

--- Assets/Astro-Stellar/Scripts/Steveo/PlayerInteract.cs
+++ Assets/Astro-Stellar/Scripts/Steveo/PlayerInteract.cs
@@ -12,21 +12,20 @@ namespace A1.Player
     public class PlayerInteract : NetworkBehaviour
     {
 	    private Camera playerCamera;
+	    [Header("Camera")]
 	    [SerializeField] private Vector3 camOffset;
-
+		[Header("Item Variables")]
 	    [SerializeField] public Item itemHolding = null;
 	    [SerializeField] public Transform itemLocation;
+	    [Header("Player Score")]
 	    public int personalScore;
 	    
-        /* pick up items
+        /* 
          *  update HUD
-         * is holding item
-         * drop item??
          * WIN
          * LOOSE ??
          */
 
-        [SerializeField] private GameObject trap;
 
 
 
@@ -39,40 +38,7 @@ namespace A1.Player
 	        
         }
 
-        // /// <summary>
-        // /// Function for dropping traps.
-        // /// </summary>
-        // [Server] // Only runs on the server.
-        // private void DropTrap()
-        // {
-	       //  // Calculate the distance to the ground from the player character.
-	       //  float dist = 0;
-	       //  Ray ray = new Ray(transform.position, Vector3.down);
-	       //  RaycastHit hit;
-	       //  if(Physics.Raycast(ray, out hit, 5f))
-	       //  {
-		      //   dist = hit.distance;
-	       //  }
-        //
-	       //  // todo will have to be moved into the networked player script.
-	       //  
-	       //  if(trapCount > 0)
-	       //  {
-		      //   // Use the calculated distance to set the position for the trap.
-		      //   Vector3 position = new Vector3(transform.position.x, transform.position.y - dist, transform.position.z);
-		      //   GameObject droppedTrap = Instantiate(trap, position, Quaternion.identity);
-		      //   // Minus 1 from the trap count.
-		      //   trapCount -= 1;
-		      //   NetworkServer.Spawn(droppedTrap);
-	       //  }
-	       //  else
-	       //  {
-		      //   Debug.Log("No Traps left.");
-		      //   // todo UI feedback, no traps. Will need to be called in an RPC.
-	       //  }
-	       //  
-	       //   
-        // }
+        
         
         // Start is called before the first frame update
         void Start()
@@ -80,28 +46,14 @@ namespace A1.Player
         
         }
 
-        // [Command]
-        // public void CmdDropTrap()
-        // {
-	       //  DropTrap();
-        // }
-
-        
         
         // Update is called once per frame
         void Update()
         {
-	        
-
 	        if(isLocalPlayer)
 	        {
 		        playerCamera.transform.position = transform.position + camOffset;
 		        playerCamera.transform.LookAt(transform.position);
-		        
-		        if(Input.GetKeyDown(KeyCode.Space))
-		        {
-			        //CmdDropTrap();
-		        }
 	        }
         }
     }
